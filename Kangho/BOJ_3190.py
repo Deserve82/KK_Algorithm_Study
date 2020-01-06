21077bbe06f6b240aa1d95e67f0318bf4c3b92fa
inth8204 <inth8204@gmail.com>
2020-01-06 14:28:58 +0900

    solve boj3190

--- Kangho/BOJ_3190.py
+++ Kangho/BOJ_3190.py
@@ -20,12 +20,10 @@ direction = 0
 for i in loa:
     F[i[0]-1][i[1]-1] = -1
 count = 0
-snake = [[0, 0]]
+snake = [0, 0]
+snake_body = []
 idx = 0
 while True:
-
-    if count > 1000:
-        break
     if idx < len(smove):
         if int(smove[idx][0]) == count:
             if smove[idx][1] == 'D':
@@ -38,85 +36,49 @@ while True:
                     direction = 3
             idx += 1
 
-    count += 1
     bf = 0
+    count += 1
 
     if direction == 0:
-        snake[0][1] += 1
-        if len(snake) > 2:
-            for i in range(2, len(snake)):
-                if snake[0] == snake[i]:
-                    bf = 1
-                    break
-        for i in range(len(snake) - 1, 0, -1):
-            snake[i][0] = snake[i - 1][0]
-            snake[i][1] = snake[i - 1][1]
-
-
-        if snake[0][1] >= size:
+        snake[1] += 1
+        if snake[1] >= size:
             bf = 1
             break
 
     elif direction == 1:
-        snake[0][0] += 1
-        if len(snake) > 2:
-            for i in range(2, len(snake)):
-                if snake[0] == snake[i]:
-                    bf = 1
-                    break
-        for i in range(len(snake) - 1, 0, -1):
-            snake[i][0] = snake[i - 1][0]
-            snake[i][1] = snake[i - 1][1]
-
-        if snake[0][0] >= size:
+        snake[0] += 1
+        if snake[0] >= size:
             bf = 1
             break
 
     elif direction == 2:
-        snake[0][1] -= 1
-        if len(snake) > 2:
-            for i in range(2, len(snake)):
-                if snake[0] == snake[i]:
-                    bf = 1
-                    break
-        for i in range(len(snake) - 1, 0, -1):
-            snake[i][0] = snake[i - 1][0]
-            snake[i][1] = snake[i - 1][1]
-
-        if snake[0][1] < 0:
+        snake[1] -= 1
+        if snake[1] < 0:
             bf = 1
             break
 
     elif direction == 3:
-        snake[0][0] -= 1
-        if len(snake) > 2:
-            for i in range(2, len(snake)):
-                if snake[0] == snake[i]:
-                    bf = 1
-                    break
-        for i in range(len(snake) - 1, 0, -1):
-            snake[i][0] = snake[i - 1][0]
-            snake[i][1] = snake[i - 1][1]
-
-        if snake[0][0] < 0:
+        snake[0] -= 1
+        if snake[0] < 0:
             bf = 1
             break
+
+    if snake in snake_body:
+        bf = 1
+    if direction == 0:
+        snake_body.append([snake[0], snake[1]-1])
+    elif direction == 1:
+        snake_body.append([snake[0]-1, snake[1]])
+    elif direction == 2:
+        snake_body.append([snake[0], snake[1]+1])
+    elif direction == 3:
+        snake_body.append([snake[0]+1, snake[1]])
+    if F[snake[0]][snake[1]] == -1:
+        F[snake[0]][snake[1]] = 0
+    else:
+        snake_body.pop(0)
 
     if bf == 1:
         break
 
-    if F[snake[0][0]][snake[0][1]] == -1:
-        F[snake[0][0]][snake[0][1]] = 0
-        if direction == 0:
-            snake.append([snake[-1][0], snake[-1][1]-1])
-        elif direction == 1:
-            snake.append([snake[-1][0]-1, snake[-1][1]])
-        elif direction == 2:
-            snake.append([snake[-1][0]+1, snake[-1][1]])
-        elif direction == 3:
-            snake.append([snake[-1][0], snake[-1][1]+1])
-
 print(count)
-
-
-
