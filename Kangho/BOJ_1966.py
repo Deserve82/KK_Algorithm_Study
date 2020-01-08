a45d4cc4a4086e804603af5c4bd3120f8b625300
inth8204 <inth8204@gmail.com>
2020-01-09 00:06:05 +0900

    print_que

--- Kangho/BOJ_1966.py
+++ Kangho/BOJ_1966.py
@@ -2,21 +2,22 @@ N = int(input())
 for i in range(N):
     aNumber, theNumber = map(int, input().split())
     importanceList = list(map(int, input().split()))
-
-answer = 0
-while theNumber:
-    for c in range(len(importanceList)):
-        if theNumber < 0:
-            break
-        if importanceList[0] < importanceList[c]:
-            if c == theNumber:
-                theNumber = len(importanceList)-1
-            temp = importanceList.pop(0)
-            importanceList.append(temp)
-            theNumber -= 1
-        else:
+    answer = 0
+    while theNumber >= 0:
+        firstPopFlag = 0
+        for c in range(len(importanceList)):
+            if importanceList[0] < importanceList[c]:
+                temp = importanceList.pop(0)
+                importanceList.append(temp)
+                theNumber -= 1
+                if theNumber < 0:
+                    theNumber = len(importanceList)-1
+                break
+            if c == len(importanceList)-1:
+                firstPopFlag = 1
+        if firstPopFlag == 1:
             importanceList.pop(0)
             theNumber -= 1
             answer += 1
 
-print(answer)
+    print(answer)
