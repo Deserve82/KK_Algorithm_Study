55d627c032d1663d6b9cabe54cff3d8e66605784
inth8204 <inth8204@gmail.com>
2020-02-04 15:17:26 +0900

    chicken delivery

--- Kangho/BOJ_15686.py
+++ Kangho/BOJ_15686.py
@@ -3,7 +3,6 @@ N, M = map(int, input().split())
 bhc_list = []
 home_list = []
 selected_bhc = []
-min_distance = 100000000
 for i in range(N):
     temp = list(map(int, input().split()))
     for j in range(N):
@@ -14,52 +13,14 @@ for i in range(N):
             temp[j] = 0
 selected_bhc = list(combinations(bhc_list, M))
 distance_list = []
-for home in home_list:
-    temp_dis = []
-    for stores in selected_bhc:
+for stores in selected_bhc:
+    two = []
+    for home in home_list:
+        one = []
         for store in stores:
-            if home[0] > store[0]:
-                temp_r = home[0] - store[0]
-            if home[0] <= store[0]:
-                temp_r = store[0] - home[0]
-            if home[1] > store[1]:
-                temp_c = home[1] - store[1]
-            if home[1] <= store[1]:
-                temp_c = store[1] - home[1]
-        temp_dis.append(temp_r+temp_c)
-    distance_list.append(min(temp_dis))
-    print(distance_list)
-if sum(distance_list) < min_distance:
-    min_distance = sum(distance_list)
-
-# def bhc_combination(idx):
-#     global N
-#     global M
-#     global min_distance
-#     if idx == M:
-#         distance_list = []
-#         for home in home_list:
-#             temp_dis = []
-#             for store in selected_bhc:
-#                 if home[0] > store[0]:
-#                     temp_r = home[0] - store[0]
-#                 if home[0] <= store[0]:
-#                     temp_r = store[0] - home[0]
-#                 if home[1] > store[1]:
-#                     temp_c = home[1] - store[1]
-#                 if home[1] <= store[1]:
-#                     temp_c = store[1] - home[1]
-#                 temp_dis.append(temp_r+temp_c)
-#             distance_list.append(min(temp_dis))
-#         if sum(distance_list) < min_distance:
-#             min_distance = sum(distance_list)
-#     else:
-#         for store in bhc_list:
-#             if store not in selected_bhc:
-#                 selected_bhc.append(store)
-#                 bhc_combination(idx+1)
-#                 selected_bhc.pop()
-#
-#
-# bhc_combination(0)
-print(min_distance)
+            temp_r = abs(home[0]-store[0])
+            temp_c = abs(home[1]-store[1])
+            one.append(temp_c + temp_r)
+        two.append(min(one))
+    distance_list.append(sum(two))
+print(min(distance_list))
