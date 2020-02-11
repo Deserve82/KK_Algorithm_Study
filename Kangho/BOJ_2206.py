b179cc197f5648a030766f5b357d548873ddc4a0
inth8204 <inth8204@gmail.com>
2020-02-11 16:53:22 +0900

    moving with breaking wall

--- Kangho/BOJ_2206.py
+++ Kangho/BOJ_2206.py
@@ -1,34 +1,36 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
 N, M = map(int, input().split())
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dx = (-1, 0, 1, 0)
+dy = (0, 1, 0, -1)
 f = []
-walls = []
-answer_list = []
 for i in range(N):
-    temp = list(map(int, input()))
-    f.append(temp)
+    f.append(list(input()))
 distance = [[[0, 0] for _ in range(M)] for _ in range(N)]
 
 
 def bfs():
-    Q = []
-    Q.append([0, 0, 0])
+    Q = deque()
+    Q.append((0, 0, 0))
     distance[0][0][0] = 1
 
     while Q:
-        location = Q.pop(0)
-        if location[0] == N-1 and location[1] == M-1:
-            return distance[location[0]][location[1]][location[2]]
+        x, y, w = Q.popleft()
+        if x == N-1 and y == M-1:
+            return distance[x][y][w]
         for i in range(4):
-            nx, ny = location[0]+dx[i], location[1]+dy[i]
+            nx, ny = x+dx[i], y+dy[i]
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-            if f[nx][ny] == 0:
-                distance[nx][ny][location[2]] = distance[location[0]][location[1]][location[2]] + 1
-                Q.append([nx, ny, location[2]])
-            if f[nx][ny] == 1 and location[2] == 0:
-                distance[nx][ny][1] = distance[location[0]][location[1]][location[2]] + 1
-                Q.append([nx, ny, 1])
+            if distance[nx][ny][w]:
+                continue
+            if f[nx][ny] == '0':
+                distance[nx][ny][w] = distance[x][y][w] + 1
+                Q.append((nx, ny, w))
+            if f[nx][ny] == '1' and w == 0:
+                distance[nx][ny][1] = distance[x][y][w] + 1
+                Q.append((nx, ny, 1))
     return -1
 
 
