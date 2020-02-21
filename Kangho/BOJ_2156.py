c06c0d50387022ec8941eb318e29ce480307648c
inth8204 <inth8204@gmail.com>
2020-02-21 11:16:55 +0900

    wine tasting

--- Kangho/BOJ_2156.py
+++ Kangho/BOJ_2156.py
@@ -1,5 +1,14 @@
 n = int(input())
-wines = []
+wines = [0]
 for _ in range(n):
     wines.append(int(input()))
+result = [0 for _ in range(n+1)]
+for i in range(1, n+1):
+    if i == 1:
+        result[1] = wines[1]
+    elif i == 2:
+        result[2] = wines[2]+wines[1]
+    else:
+        result[i] = max(result[i-3] + wines[i-1] + wines[i], result[i-2] + wines[i], result[i-1])
 
+print(result[n])
