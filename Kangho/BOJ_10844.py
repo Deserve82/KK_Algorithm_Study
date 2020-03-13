9505fb6f7a77ecf25da8b6fff91bbf61af92ba5d
Kangho LEE <54825653+Deserve82@users.noreply.github.com>
2020-03-13 23:37:09 +0900

    climbing easy stairs

--- Kangho/BOJ_10844.py
+++ Kangho/BOJ_10844.py
@@ -1,13 +1,13 @@
 n = int(input())
-dp = [[0 for i in range(10)] for j in range(101)]
+dps = [[0 for i in range(10)] for j in range(101)]
 for i in range(1, 10):
-    dp[1][i] = 1
+    dps[1][i] = 1
 for i in range(2, n + 1):
     for j in range(10):
         if j == 0:
-            dp[i][j] = dp[i - 1][1]
+            dps[i][j] = dps[i - 1][1]
         elif j == 9:
-            dp[i][j] = dp[i - 1][8]
+            dps[i][j] = dps[i - 1][8]
         else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-print(sum(dp[n]) % 1000000000)
+            dps[i][j] = dps[i - 1][j - 1] + dps[i - 1][j + 1]
+print(sum(dps[n]) % 1000000000)
