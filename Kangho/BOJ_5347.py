7fd2eada14ec5222971eb4877277888a04f40412
Kangho Lee <inth8204@gmail.com>
2020-03-16 21:27:25 +0900

    lcm

---
+++ Kangho/BOJ_5347.py
@@ -0,0 +1,11 @@
+from math import gcd
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+
+testcase = int(input())
+for _ in range(testcase):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
