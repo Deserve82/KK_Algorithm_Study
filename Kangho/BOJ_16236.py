673c3628fe11a8d58871e7fd089852c5d1f98af5
deserve82 <inth8204@gmail.com>
2020-04-26 00:34:09 +0900

    baby shark

--- Kangho/BOJ_16236.py
+++ Kangho/BOJ_16236.py
@@ -18,7 +18,7 @@ def search_fish(i, j):
     fish_tank = []
     dis = copy.deepcopy(dis_list)
     temp_sea = copy.deepcopy(sea)
-    temp_sea[i][j] = 10
+    temp_sea[i][j] = 0
     while q:
         location = q.pop(0)
         row = location[0]
@@ -30,7 +30,8 @@ def search_fish(i, j):
             if m_row < 0 or m_row > n - 1 or m_column < 0 or m_column > n - 1:
                 pass
             else:
-                if sea[m_row][m_column] == shark_size or sea[m_row][m_column] == 0:
+                dis[m_row][m_column] = dis[row][column] + 1
+                if sea[m_row][m_column] == shark_size or sea[m_row][m_column] == 0 or sea[m_row][m_column] == 9:
                     if temp_sea[m_row][m_column] == 10:
                         pass
                     else:
@@ -38,8 +39,29 @@ def search_fish(i, j):
                         temp_sea[m_row][m_column] = 10
                 elif sea[m_row][m_column] < shark_size:
                     temp_sea[m_row][m_column] = 10
-                    fish_tank.append([m_row, m_column])
+                    fish_tank.append([m_row, m_column, dis[m_row][m_column]])
 
+    value = 100000
+    fish_row = i
+    fish_column = j
+    for fish in fish_tank:
+        if fish[2] < value:
+            value = fish[2]
+            fish_row = fish[0]
+            fish_column = fish[1]
+        elif fish[2] == value:
+            if fish[0] < fish_row:
+                fish_row = fish[0]
+                fish_column = fish[1]
+            elif fish[0] == fish_row:
+                if fish[1] < fish_column:
+                    fish_column = fish[1]
+    sea[fish_row][fish_column] = 0
+    eaten_fishes += 1
+    if shark_size == eaten_fishes:
+        eaten_fishes = 0
+        shark_size += 1
+    return [fish_row, fish_column, value]
 
 
 time = 0
@@ -55,6 +77,5 @@ for i in range(n):
                 else:
                     shark_loc = result
                     time += result[2]
-                    # print(time, shark_size)
             break
 print(time)
