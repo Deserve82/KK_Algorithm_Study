43f7ac126f99aef5bf3eb68134998de88191d886
Kangho LEE <inth8204@gmail.com>
2020-09-07 23:45:23 +0900

    remote control
    code from https://br-brg.tistory.com/19

--- Kangho/BOJ_1107.py
+++ Kangho/BOJ_1107.py
@@ -1,63 +1,31 @@
-import math
-
-
-def min_checker(number):
-    if buttons:
-        mv = 10
-        n = 0
-        for b in buttons:
-            if abs(b-number) < mv:
-                mv = abs(b-number)
-                n = b
-        return n
-
-
-def make_max_and_move(want, l):
-    val = [str(buttons[0])]*l
-    w = want
-    mv = 50000001
-    for i in range(l, 0, -1):
-        for b in buttons:
-            if abs(w - b*math.pow(10, i-1)) < mv:
-                mv = abs(w - b*math.pow(10, i-1))
-                val[l-i] = str(b)
-        if len(str(w)) > 1:
-            w = int(str(w)[1:])
-        else:
-            return ''.join(val)
-    return ''.join(val)
-
-
-want_c = int(input())
-l = len(str(want_c))
 n = int(input())
-del_buttons = list(map(int, input().split()))
-buttons = [i for i in range(10)]
-c = 100
-for d in del_buttons:
-    buttons.remove(d)
-
-answer = 5000001
-if buttons:
-    pivot = ''
-    a = 0
-    for wc in str(want_c):
-        pivot += str(min_checker(int(wc)))
-        a += 1
-    if pivot != str(want_c):
-        a += abs(int(pivot)-want_c)
-    if a < answer:
-        answer = a
-    t = int(make_max_and_move(want_c, l))
-    if abs(want_c - t) + l < answer:
-        answer = abs(want_c - t) + l
-    if l > 1:
-        t = int(make_max_and_move(want_c, l-1))
-        if abs(want_c - t) + l-1 < answer:
-            answer = abs(want_c - t) + l-1
-    t = int(make_max_and_move(want_c, l+1))
-    if abs(t - want_c) + l + 1 < answer:
-        answer = abs(t - want_c) + l + 1
-if abs(want_c-c) < answer:
-    answer = abs(want_c-c)
+m = int(input())
+if m > 0:
+    malf = input().split()
+else:
+    malf = []
+ 
+channels = []
+curr = 100
+ 
+for c in range(0, 1000000+1):
+    isCont = True
+    for str_c in str(c):
+        if str_c in malf:
+            isCont = False
+            break
+    if isCont:
+        channels.append(c)
+ 
+min_diff = 987654321
+val = ''
+for i in channels:
+    if abs(i-n) < min_diff:
+        min_diff = abs(i-n)
+        val = str(i)
+ 
+answer = len(val)+abs(min_diff)
+ 
+answer = min(abs(n-curr), answer)
+ 
 print(answer)
