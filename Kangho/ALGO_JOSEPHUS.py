491e9ac6f763ab14ec23b7c6523a7fca99a4a14c
Kangho LEE <inth8204@gmail.com>
2020-12-25 15:56:42 +0900

    josephus

--- Kangho/ALGO_JOSEPHUS.py
+++ Kangho/ALGO_JOSEPHUS.py
@@ -14,3 +14,25 @@ if __name__ == '__main__':
         army = [str(a) for a in range(1, n+1)]
         josepus()
         print(" ".join(army))
+        
+        
+# queue를 이용한 풀이
+from collections import deque
+
+
+def josepus():
+    q = deque(army)
+    m = n
+    while m != 2:
+        q.popleft()
+        m -= 1
+        for _ in range(k - 1):
+            q.append(q.popleft())
+    return q
+
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+        army = [str(a) for a in range(1, n + 1)]
+        print(" ".join(josepus()))
