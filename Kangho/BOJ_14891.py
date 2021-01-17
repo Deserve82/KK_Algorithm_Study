e7681f7d4144320432d61300d6cb3f91800b3aa7
Kangho LEE <inth8204@gmail.com>
2021-01-17 11:23:28 +0900

    톱니바퀴

--- Kangho/BOJ_14891.py
+++ Kangho/BOJ_14891.py
@@ -1,122 +1,57 @@
-gear = []
-rotate = []
-answer = 0
+def move(gear_num, is_clockwise):
+    if is_clockwise:
+        gears[gear_num] = gears[gear_num][-1] + gears[gear_num][:-1]
+    else:
+        gears[gear_num] = gears[gear_num][1:] + gears[gear_num][0]
+
+
+def where_rotate(start, is_cloclwise):
+    should_rotate = []
+    t = start
+    tc = is_cloclwise
+    while t > 0:
+        if gears[t][6] != gears[t-1][2]:
+            should_rotate.append([t - 1, not tc])
+            tc = not tc
+            t -= 1
+        else:
+            break
+    t = start
+    tc = is_cloclwise
+    while t < 3:
+        if gears[t][2] != gears[t+1][6]:
+            should_rotate.append([t + 1, not tc])
+            tc = not tc
+            t += 1
+        else:
+            break
+    return should_rotate
+
+
+gears = []
+for _ in range(4):
+    gears.append(input())
+N = int(input())
+commands = []
+for _ in range(N):
+    a, b = map(int, input().split())
+    commands.append([a-1, b])
+for g, c in commands:
+    if c == 1:
+        sr = where_rotate(g, True)
+        move(g, True)
+        for s in sr:
+            move(s[0], s[1])
+    else:
+        sr = where_rotate(g, False)
+        move(g, False)
+        for s in sr:
+            move(s[0], s[1])
+points = [1, 2, 4, 8]
+ans = 0
 for i in range(4):
-    gear.append(list(input()))
-rotate_number = int(input())
-for i in range(rotate_number):
-    temp = list(map(int, input().split()))
-    rotate.append(temp)
-
-
-def rotating(a, dir):
-    if dir == 1:
-        t = a[-1]
-        for i in range(len(a)-1, 0, -1):
-            a[i] = a[i-1]
-        a[0] = t
+    if gears[i][0] == '1':
+        ans += points[i]
     else:
-        t = a.pop(0)
-        a.append(t)
-
-def d(a):
-    if a == 1:
-        return -1
-    else:
-        return 1
-
-
-def how_many_gear_rotate(gear):
-    move = [0, 0, 0]
-    for g in range(3):
-        if gear[g][2] != gear[g+1][6]:
-            move[g] = 1
-    return move
-
-
-for r in rotate:
-    m = how_many_gear_rotate(gear)
-    if r[0] == 1 and m[0] == 0:
-        rotating(gear[0], r[1])
-    elif r[0] == 1 and m[0] == 1 and m[1] == 0:
-        rotating(gear[0], r[1])
-        rotating(gear[1], d(r[1]))
-    elif r[0] == 1 and m[0] == 1 and m[1] == 1 and m[2] == 0:
-        rotating(gear[0], r[1])
-        rotating(gear[1], d(r[1]))
-        rotating(gear[2], r[1])
-    elif r[0] == 1 and m[0] == 1 and m[1] == 1 and m[2] == 1:
-        rotating(gear[0], r[1])
-        rotating(gear[1], d(r[1]))
-        rotating(gear[2], r[1])
-        rotating(gear[3], d(r[1]))
-
-    if r[0] == 2 and m[0] == 0 and m[1] == 0:
-        rotating(gear[1], r[1])
-    elif r[0] == 2 and m[0] == 0 and m[1] == 1 and m[2] == 0:
-        rotating(gear[1], r[1])
-        rotating(gear[2], d(r[1]))
-    elif r[0] == 2 and m[0] == 0 and m[1] == 1 and m[2] == 1:
-        rotating(gear[1], r[1])
-        rotating(gear[2], d(r[1]))
-        rotating(gear[3], r[1])
-    elif r[0] == 2 and m[0] == 1 and m[1] == 0:
-        rotating(gear[1], r[1])
-        rotating(gear[0], d(r[1]))
-    elif r[0] == 2 and m[0] == 1 and m[1] == 1 and m[2] == 0:
-        rotating(gear[1], r[1])
-        rotating(gear[0], d(r[1]))
-        rotating(gear[2], d(r[1]))
-    elif r[0] == 2 and m[0] == 1 and m[1] == 1 and m[2] == 1:
-        rotating(gear[1], r[1])
-        rotating(gear[0], d(r[1]))
-        rotating(gear[2], d(r[1]))
-        rotating(gear[3], r[1])
-
-    if r[0] == 3 and m[2] == 0 and m[1] == 0:
-        rotating(gear[2], r[1])
-    elif r[0] == 3 and m[2] == 0 and m[1] == 1 and m[0] == 0:
-        rotating(gear[2], r[1])
-        rotating(gear[1], d(r[1]))
-    elif r[0] == 3 and m[2] == 0 and m[1] == 1 and m[0] == 1:
-        rotating(gear[2], r[1])
-        rotating(gear[1], d(r[1]))
-        rotating(gear[0], r[1])
-    elif r[0] == 3 and m[2] == 1 and m[1] == 0:
-        rotating(gear[2], r[1])
-        rotating(gear[3], d(r[1]))
-    elif r[0] == 3 and m[2] == 1 and m[1] == 1 and m[0] == 0:
-        rotating(gear[2], r[1])
-        rotating(gear[1], d(r[1]))
-        rotating(gear[3], d(r[1]))
-    elif r[0] == 3 and m[2] == 1 and m[1] == 1 and m[0] == 1:
-        rotating(gear[2], r[1])
-        rotating(gear[1], d(r[1]))
-        rotating(gear[3], d(r[1]))
-        rotating(gear[0], r[1])
-
-    if r[0] == 4 and m[2] == 0:
-        rotating(gear[3], r[1])
-    elif r[0] == 4 and m[2] == 1 and m[1] == 0:
-        rotating(gear[3], r[1])
-        rotating(gear[2], d(r[1]))
-    elif r[0] == 4 and m[2] == 1 and m[1] == 1 and m[0] == 0:
-        rotating(gear[3], r[1])
-        rotating(gear[2], d(r[1]))
-        rotating(gear[1], r[1])
-    elif r[0] == 4 and m[0] == 1 and m[1] == 1 and m[2] == 1:
-        rotating(gear[3], r[1])
-        rotating(gear[2], d(r[1]))
-        rotating(gear[1], r[1])
-        rotating(gear[0], d(r[1]))
-
-if gear[0][0] == '1':
-    answer += 1
-if gear[1][0] == '1':
-    answer += 2
-if gear[2][0] == '1':
-    answer += 4
-if gear[3][0] == '1':
-    answer += 8
-
-print(answer)
+        continue
+print(ans)
