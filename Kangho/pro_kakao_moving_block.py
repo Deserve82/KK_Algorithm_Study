578209539898ad94b7ef39329a0f2da7fd685414
Kangho LEE <inth8204@gmail.com>
2021-01-19 10:02:59 +0900

    블록 이동하기

--- Kangho/pro_kakao_moving_block.py
+++ Kangho/pro_kakao_moving_block.py
@@ -1,56 +1,63 @@
 from collections import deque
+movements = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+rotations = [1, -1]
 
 
-# 1초동안 움직일 수 있는 모든 경우
-def move(cor1, cor2, board):
-    move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    ret = []
-    # 이동
-    for m in move:
-        if board[cor1[0] + m[0]][cor1[1] + m[1]] == 0 and board[cor2[0] + m[0]][cor2[1] + m[1]] == 0:
-            ret.append({(cor1[0] + m[0], cor1[1] + m[1]), (cor2[0] + m[0], cor2[1] + m[1])})
+def move(t, h, nb):
+    value_lists = []
+    tr, tc = t
+    hr, hc = h
+    for i in range(4):
+        mr, mc = movements[i]
+        if nb[tr+mr][tc+mc] == 0 and nb[hr+mr][hc+mc] == 0:
+            value_lists.append(((tr+mr, tc+mc), (hr+mr, hc+mc)))
 
-    rotate = [1, -1]
-    # 가로회전
-    if cor1[0] == cor2[0]:
-        for r in rotate:
-            if board[cor1[0] + r][cor1[1]] == 0 and board[cor2[0] + r][cor2[1]] == 0:
-                ret.append({(cor1[0] + r, cor1[1]), (cor1[0], cor1[1])})
-                ret.append({(cor2[0] + r, cor2[1]), (cor2[0], cor2[1])})
-    # 세로회전
-    else:
-        for r in rotate:
-            if board[cor1[0]][cor1[1] + r] == 0 and board[cor2[0]][cor2[1] + r] == 0:
-                ret.append({(cor1[0], cor1[1]), (cor1[0], cor1[1] + r)})
-                ret.append({(cor2[0], cor2[1]), (cor2[0], cor2[1] + r)})
-    return ret
+    for i in range(2):
+        side = rotations[i]
+        if hr == tr:
+            if nb[tr+side][tc] == 0 and nb[hr+side][hc] == 0:
+                value_lists.append(((tr, tc), (tr+side, tc)))
+                value_lists.append(((hr, hc), (hr+side, hc)))
+        else:
+            if nb[tr][tc+side] == 0 and nb[hr][hc+side] == 0:
+                value_lists.append(((tr, tc+side), (tr, tc)))
+                value_lists.append(((hr, hc), (hr, hc+side)))
+    return value_lists
+
+
+def bfs(b, pivot):
+    q = deque()
+    q.append(((1, 1), (1, 2), 0))
+    check = {((1, 1), (1, 2)): 1}
+    while q:
+        tail, head, cost = q.popleft()
+
+        if tail == (pivot, pivot) or head == (pivot, pivot):
+            return cost
+
+        ml = move(tail, head, b)
+        for a in ml:
+            if a not in check:
+                check[a] = 1
+                t, h = a
+                q.append((t, h, cost+1))
 
 
 def solution(board):
-    size = len(board)
-    # 경계 체크 쉽게하기 위해서 지도의 상하좌우에 1 추가
-    new_board = [[1 for i in range(len(board) + 2)] for i in range(len(board) + 2)]
-    for i in range(len(board)):
-        for j in range(len(board)):
-            new_board[i + 1][j + 1] = board[i][j]
+    N = len(board)
+    new_board = [[0]*(N+2) for _ in range(N+2)]
 
-    que = deque()
-    visited = []
+    for i in range(N):
+        for j in range(N):
+            new_board[i+1][j+1] = board[i][j]
+    for i in range(N+2):
+        new_board[0][i] = 1
+        new_board[N+1][i] = 1
+        new_board[i][0] = 1
+        new_board[i][N+1] = 1
 
-    # queue에 [로봇의 좌표정보, 지금까지 거리] 형태로 넣음
-    que.append([{(1, 1), (1, 2)}, 0])
-    visited.append({(1, 1), (1, 2)})
+    answer = bfs(new_board, N)
+    return answer
 
-    while len(que) != 0:
-        temp = que.popleft()
-        cor = list(temp[0])
-        dist = temp[1] + 1
 
-        for m in move(cor[0], cor[1], new_board):
-            if (size, size) in m: return dist
-
-            if not m in visited:
-                que.append([m, dist])
-                visited.append(m)
-
-    return 0
+print(solution([[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]))
