f4733b1fe369744adfff07ac23bd80266ee49d59
Kangho LEE <inth8204@gmail.com>
2021-01-29 22:34:04 +0900

    자물쇠와 열쇠

--- Kangho/pro_kakao_lock_and_key.py
+++ Kangho/pro_kakao_lock_and_key.py
@@ -1,40 +1,52 @@
-def rotate_90(m):
-    N = len(m)
-    ret = [[0] * N for _ in range(N)]
-
-    for r in range(N):
-        for c in range(N):
-            ret[c][N-1-r] = m[r][c]
-    return ret
+from copy import deepcopy
 
 
-def fit_check(startX, startY, key, lock, expandSize, start, end):
-    expandList = [[0] * expandSize for _ in range(expandSize)]
+def add_key(row, col, K, nb, key):
+    for i in range(K):
+        for j in range(K):
+            nb[i + row][j + col] += key[i][j]
 
-    for i in range(len(key)):
-        for j in range(len(key)):
-            expandList[startX + i][startY+j] += key[i][j]
 
-    for i in range(start, end):
-        for j in range(start, end):
-            expandList[i][j] += lock[i-start][j-start]
-            if expandList[i][j] != 1:
+def deduct_key(row, col, K, nb, key):
+    for i in range(K):
+        for j in range(K):
+            nb[i + row][j + col] -= key[i][j]
+
+
+def check(K, L, board):
+    for i in range(K - 1, K - 1 + L):
+        for j in range(K - 1, K - 1 + L):
+            if board[i][j] != 1:
                 return False
     return True
 
 
 def solution(key, lock):
-    start = len(key) - 1
-    end = start + len(lock)
-    expandSize = len(lock) + start*2
-
-    for a in range(4):
-        for i in range(end):
-            for j in range(end):
-                if fit_check(i, j, key, lock, expandSize, start, end):
-                    return True
-        key = rotate_90(key)
-    return False
+    answer = False
+    K, L = len(key), len(lock)
+    new_board = [[0] * ((2 * K + L) - 1) for _ in range((2 * K + L) - 1)]
+    NL = len(new_board)
+    for i in range(L):
+        for j in range(L):
+            new_board[K - 1 + i][K - 1 + j] = lock[i][j]
+    keys = [deepcopy(key)]
+    for k in range(1, 4):
+        a = [[0] * K for _ in range(K)]
+        for i in range(K):
+            for j in range(K):
+                a[j][K - 1 - i] = keys[k - 1][i][j]
+        keys.append(deepcopy(a))
+    for rek in keys:
+        for i in range(NL-K):
+            for j in range(NL-K):
+                add_key(i, j, K, new_board, rek)
+                if check(K, L, new_board):
+                    answer = True
+                    break
+                deduct_key(i, j, K, new_board, rek)
+            if answer:
+                break
+    return answer
 
 
 print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
